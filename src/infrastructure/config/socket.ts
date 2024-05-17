95410efb36acc56bf5bd49cc33b574cc61e96960
Nisana M <nisana686@gmail.com>
2024-05-17 16:54:28 +0530

    cors origin added in socket

--- src/infrastructure/config/socket.ts
+++ src/infrastructure/config/socket.ts
@@ -1,6 +1,4 @@
 import { Server, Socket } from 'socket.io'
-import http from 'http'
-import { createServer } from './app'
 import ProfModel from '../database/profModel'
 
 interface User {
@@ -13,7 +11,7 @@ function socketServer(server: any) {
 
     const io = new Server(server, {
         cors: {
-            origin: ['http://localhost:5173'],
+            origin: ['http://localhost:5173','https://crafto-one.vercel.app'],
             methods: ['GET', 'POST']
         }
     });
