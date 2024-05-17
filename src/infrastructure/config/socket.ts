8ac521d214b293b472be1c61412fdfa80f8d0079
Nisana M <nisana686@gmail.com>
2024-05-17 17:19:59 +0530

    bug fixed

--- src/infrastructure/config/socket.ts
+++ src/infrastructure/config/socket.ts
@@ -11,7 +11,7 @@ function socketServer(server: any) {
 
     const io = new Server(server, {
         cors: {
-            origin: ['http://localhost:5173','https://crafto-one.vercel.app'],
+            origin: 'https://crafto-one.vercel.app',
             methods: ['GET', 'POST'],
             credentials: true
         }
