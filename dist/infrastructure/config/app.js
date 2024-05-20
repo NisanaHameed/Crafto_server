53c6f820f1deefd615693ddec7ddf19617c5b15c
Nisana M <nisana686@gmail.com>
2024-05-20 15:10:42 +0530

    public folder in dist created

--- dist/infrastructure/config/app.js
+++ dist/infrastructure/config/app.js
@@ -20,7 +20,8 @@ const createServer = () => {
         app.use(express_1.default.urlencoded({ extended: true }));
         app.use((0, cookie_parser_1.default)());
         app.use((0, cors_1.default)({
-            origin: 'https://crafto-one.vercel.app',
+            // origin: 'https://crafto-one.vercel.app',
+            origin: 'http://localhost:5173',
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
             credentials: true
         }));
