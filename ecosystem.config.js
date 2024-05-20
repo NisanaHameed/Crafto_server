02c445df6257ef89b8b736bed74bc992bdaa19cc
Nisana M <nisana686@gmail.com>
2024-05-20 15:01:43 +0530

    filename changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: "crafto",
+            name: "crafto_server",
             script: "./dist/server.js",
             instances: 1,
             autorestart: true,
